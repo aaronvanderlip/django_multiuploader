01d6c1ed8e6f62ade02e074b11e53eca0c4969c8
aaronvanderlip <avanderlip@gmail.com>
2013-05-01 11:07:53 -0400

    Move assest to static.

---
+++ setup.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+from setuptools import find_packages
+
+setup(
+    name='django-jquery-multiuploader',
+    version='0.1',
+    author=u'Aaron VanDerlip',
+    author_email='avanderlip@gmail.com',
+    packages=find_packages(),
+    url='',
+    license='BSD license',
+    description='Adds jQuery File Upload support',
+    long_description=open('README.rst').read(),
+    include_package_data=True,
+    install_requires=['django>= 1.3',],
+    zip_safe=False,
+)
